f580b1c32c9c495bbdbba4c04685f52905ac57ac
mataramis <buattryout44@gmail.com>
2023-08-18 15:46:05 +0700

    update 18 8

--- project 05 - function while/caesar cipher/vipher pool.py
+++ project 05 - function while/caesar cipher/vipher pool.py
@@ -1,56 +1,36 @@
-abjad = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-pilihan = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text= input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+  print(f"Here's the {cipher_direction}d result: {end_text}")
 
-# def encrypt (plain_text, shift_amount):
+from art import logo
+print(logo)
 
-#     cipher_text = ""
+should_end = False
+while not should_end:
 
-#     for i in plain_text :
-#         posisi_awal = abjad.index(i)
-#         posisi_baru = posisi_awal + shift_amount
-#         kata_baru = abjad[posisi_baru]
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+ 
+  shift = shift % 26
 
-#         cipher_text += kata_baru
-#     print(f"The encoded text is {cipher_text}")
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
 
-# def balik (plain_text, shift_amount):
-
-#     cipher_text = ""
-
-#     for i in plain_text :
-#         posisi_awal = abjad.index(i)
-#         posisi_baru = posisi_awal - shift_amount
-#         kata_baru = abjad[posisi_baru]
-
-#         cipher_text += kata_baru
-#     print(f"The encoded text is {cipher_text}")
-
-
-
-# if pilihan == "encode":
-#     encrypt(plain_text=text, shift_amount=shift)
-# elif pilihan == "decode":
-#     balik(plain_text=text, shift_amount=shift)
-
-
-def caesar (a_pilihan, a_text, a_shift):
-
-    a_pilihan == pilihan
-    a_text == text
-    a_shift == shift
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
+    should_end = True
+    print("Goodbye")
     
-    cipher_text = ""
 
-    
-    for i in a_text :
-        posisi_awal = abjad.index(i)
-        posisi_baru = posisi_awal + a_shift
-        kata_baru = abjad[posisi_baru]
 
-        cipher_text += kata_baru
-    print(f"The encoded text is : {cipher_text}")
-
-caesar(pilihan, text, shift)
---
+++ project 6 - dic/Score_grades.py
@@ -0,0 +1,23 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+
+student_grades = {}
+
+for i in student_scores :
+    score = student_scores[i]
+    if score > 90 :
+         student_grades[i] = "Outstanding"
+    elif score > 80 :
+         student_grades[i] = "Exceeds Expectations"
+    elif score > 70 :
+          student_grades[i] = "Acceptable"
+    else :
+         student_grades[i] = "Fail"
+    
+
+print(student_grades)
